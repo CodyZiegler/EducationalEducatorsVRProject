102f9d11ce57e8413890f05a4cda858d7b6bf913
IGlave <revenatxknight@gmail.com>
2020-02-27 12:03:49 -0500

    Scene For A.P.
    Asset only folder of the amsement park scene and modules to test if this pushes

---
+++ Assets/Add-Ons/BNG Framework/Scripts/Components/CollisionSound.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BNG {
+
+    /// <summary>
+    /// Plays a Sound Clip OnCollisionEnter
+    /// </summary>
+    public class CollisionSound : MonoBehaviour {
+
+        public AudioClip CollisionAudio;
+        AudioSource audioSource;
+        float startTime;
+
+        Collider col;
+
+        void Start() {
+            audioSource = GetComponent<AudioSource>();
+            startTime = Time.time;
+            col = GetComponent<Collider>();
+        }
+
+        private void OnCollisionEnter(Collision collision) {
+
+            // Just spawned, don't fire collision sound immediately
+            if(Time.time - startTime < 0.1f) {
+                return;
+            }
+
+            // No Collider present, don't play sound
+            if(!col.enabled) {
+                return;
+            }
+
+            if(audioSource && CollisionAudio) {
+                // Play Shot
+                if (audioSource.isPlaying) {
+                    audioSource.Stop();
+                }
+
+                audioSource.clip = CollisionAudio;
+                audioSource.pitch = Time.timeScale;
+                audioSource.Play();
+            }
+        }
+    }
+}
---
+++ Assets/Add-Ons/BNG Framework/Scripts/Components/ConstrainLocalPosition.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BNG {
+
+    /// <summary>
+    /// Constrain a Transform's LocalPosition to a given value.
+    /// </summary>
+    public class ConstrainLocalPosition : MonoBehaviour {
+
+        public bool ConstrainLocalX = false;
+        public float LocalXMin = -1f;
+        public float LocalXMax = 1f;
+
+        public bool ConstrainLocalY = false;
+        public float LocalYMin = -1f;
+        public float LocalYMax = 1f;
+
+        public bool ConstrainLocalZ = false;
+        public float LocalZMin = -1f;
+        public float LocalZMax = 1f;
+
+        void Update() {
+
+            // Save a lookup
+            if(!ConstrainLocalX && !ConstrainLocalY && !ConstrainLocalZ) {
+                return;
+            }
+
+            Vector3 currentPos = transform.localPosition;
+            float newX = ConstrainLocalX ? Mathf.Clamp(currentPos.x, LocalXMin, LocalXMax) : currentPos.x;
+            float newY = ConstrainLocalX ? Mathf.Clamp(currentPos.x, LocalXMin, LocalXMax) : currentPos.x;
+            float newZ = ConstrainLocalX ? Mathf.Clamp(currentPos.x, LocalXMin, LocalXMax) : currentPos.x;
+
+            transform.localPosition = new Vector3(newX, newY, newZ);
+        }
+    }
+}
+
