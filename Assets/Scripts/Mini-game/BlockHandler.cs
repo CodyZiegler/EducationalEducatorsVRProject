5c91de03803f1995314d06d671acb9e0d2c533a2
IGlave <revenant.david@gmail.com>
2020-04-16 21:30:35 -0400

    REVERTING CHANGES

--- Assets/Scripts/Mini-game/BlockHandler.cs
+++ Assets/Scripts/Mini-game/BlockHandler.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using BNG;
 
-[RequireComponent(typeof(AudioSource))]
 public class BlockHandler : MonoBehaviour
 {
     public List<GameObject> block = new List<GameObject>();
@@ -15,9 +14,6 @@ public class BlockHandler : MonoBehaviour
     private List<Vector3> vecs;
     private List<Vector3> tempVecs = new List<Vector3>();
 
-    private AudioSource audioPlayer;
-    public AudioClip rightSound, wrongSound;
-
     void Start()
     {
         input = GetComponent<InputBridge>();
@@ -25,8 +21,7 @@ public class BlockHandler : MonoBehaviour
                                    new Vector3(-.5f, 1f, .2f), new Vector3(-.5f, 1f, -.2f), new Vector3(.2f, 1f, -.5f), new Vector3(-.2f, 1f, -.5f)};
         populateTempBlock();
         GenerateBlocks();
-
-        audioPlayer = gameObject.GetComponent<AudioSource>();
+        
     }
 
     private void Update()
@@ -76,21 +71,7 @@ public class BlockHandler : MonoBehaviour
         }
     }
 
-    IEnumerator CorrectPlacement() {
-        GameObject[] allBlocks = GameObject.FindGameObjectsWithTag("Block");
-        ParticleSystem p;
-        foreach (GameObject b in allBlocks)
-        {
-            if (b.TryGetComponent<ParticleSystem>(out p))
-            {
-                p.Play();
-            }
-        }
-        audioPlayer.clip = rightSound;
-        audioPlayer.Play();
-
-        yield return new WaitForSeconds(2.0f);
-
+    void CorrectPlacement() {
         populateTempBlock();
         ClearBlocks();
         numOfBlocks++;
@@ -98,9 +79,6 @@ public class BlockHandler : MonoBehaviour
     }
 
     void WrongPlacements() {
-        audioPlayer.clip = wrongSound;
-        audioPlayer.Play();
-
         // This will need to output to a file both the time and amount of levels correct.
     }
 
@@ -125,7 +103,7 @@ public class BlockHandler : MonoBehaviour
         }
         if (blocksOK)
         {
-            StartCoroutine(CorrectPlacement());
+            CorrectPlacement();
         }
         else {
             WrongPlacements();
